9ec3a0b360fb018377803801760a4652bfb65a6c
Aaryan Pradhan <pradhanaaryan@gmail.com>
2026-02-28 17:16:37 +0530

    Spring has Booted

---
+++ src/main/java/com/aaryan/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.aaryan.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Aaryan, Spring Boot is running!";
+    }
+}
