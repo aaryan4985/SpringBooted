260347012596cbd725cb2fb90326368511dc8675
Aaryan Pradhan <pradhanaaryan@gmail.com>
2026-02-28 17:24:28 +0530

    hello controllers

--- src/main/java/com/aaryan/demo/HelloController.java
+++ src/main/java/com/aaryan/demo/HelloController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
     @GetMapping("/")
     public String hello() {
-        return "Hello Aaryan, Spring Boot is running!";
+        return helloService.getMessage();
     }
 }
