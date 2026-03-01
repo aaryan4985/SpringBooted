036875b6cc286a243f77767b3067651fa026aabf
Aaryan Pradhan <pradhanaaryan@gmail.com>
2026-03-01 12:46:54 +0530

    service

---
+++ src/main/java/com/aaryan/demo/Student.java
@@ -0,0 +1,42 @@
+package com.aaryan.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
