96772dc797b7eaa4ef53c7d58f1923d5d6773744
Aaryan Pradhan <pradhanaaryan@gmail.com>
2026-03-02 09:57:37 +0530

    student func

---
+++ src/main/java/com/aaryan/demo/StudentController.java
@@ -0,0 +1,30 @@
+package com.aaryan.demo;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.saveStudent(student);
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+    }
+}
